105503f3e7d31d97ad038aa907219f41d6a27eeb
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-08-25 19:44:38 -0500

    Build list of product with JSON

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
+import data from '../../db.json'
+
 export default function Home() {
+  const product = data.products[9]
+  const image = product.image
   return (
     <main>
       <h1>Roger David Alba Ortega</h1>
+      <section>
+        <h2>{product.name}</h2>
+        <Image
+          src={image}
+          alt="h"
+          width={200}
+          height={200}
+          loading="lazy"
+        ></Image>
+      </section>
     </main>
   )
 }
